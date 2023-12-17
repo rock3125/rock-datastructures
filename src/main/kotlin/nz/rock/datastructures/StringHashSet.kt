e3283f0eeb64fba0c272e602cf92f79faebd0589
Rock de Vocht <rock@simsage.co.uk>
2023-12-17 11:02:03 +0000

    changed long array to int array

--- src/main/kotlin/nz/rock/datastructures/StringHashSet.kt
+++ src/main/kotlin/nz/rock/datastructures/StringHashSet.kt
@@ -17,7 +17,7 @@ import kotlin.math.absoluteValue
 /**
  * a memory efficient mostly accurate String hash set
  * checking for the presence / existence of a string
- * based on double hash values
+ * based on double hash values 0 allocating 20 bytes per string total
  *
  */
 class StringHashSet(private val initialSize: Int) {
@@ -25,11 +25,11 @@ class StringHashSet(private val initialSize: Int) {
     // an array of firsts - the first offsets into hash
     private var first = IntArray(initialSize) { EMPTY_KEY }
 
-    // an array of the long hash values for size-remapping
-    private var longHash = LongArray(initialSize) { EMPTY_KEY_LONG }
+    // an array of type 1 hashes identifying a string
+    private var intHash1 = IntArray(initialSize) { EMPTY_KEY }
 
-    // an array of hashes identifying a string
-    private var intHash = IntArray(initialSize) { EMPTY_KEY }
+    // an array of type 2 hashes identifying a string
+    private var intHash2 = IntArray(initialSize) { EMPTY_KEY }
 
     // an array of next offsets for collisions
     private var next = IntArray(initialSize) { EMPTY_KEY }
@@ -46,14 +46,14 @@ class StringHashSet(private val initialSize: Int) {
         // shrink the arrays?
         if (first.size > initialSize) {
             first = IntArray(initialSize) { EMPTY_KEY }
-            longHash = LongArray(initialSize) { EMPTY_KEY_LONG }
-            intHash = IntArray(initialSize) { EMPTY_KEY }
+            intHash1 = IntArray(initialSize) { EMPTY_KEY }
+            intHash2 = IntArray(initialSize) { EMPTY_KEY }
             next = IntArray(initialSize) { EMPTY_KEY }
         } else {
             first.fill(EMPTY_KEY)
-            longHash.fill(EMPTY_KEY_LONG)
+            intHash2.fill(EMPTY_KEY)
             next.fill(EMPTY_KEY)
-            intHash.fill(EMPTY_KEY)
+            intHash1.fill(EMPTY_KEY)
         }
     }
 
@@ -87,10 +87,10 @@ class StringHashSet(private val initialSize: Int) {
         }
 
         // get an index into the first array
-        val longHashValue = stringToHashLong(str)
-        val intHashValue = stringToHashInt(str)
+        val intHash1Value = stringToHash1(str)
+        val intHash2Value = stringToHash2(str)
         val oldSize = size
-        size = insertHelper(longHashValue, intHashValue, size, first, longHash, intHash, next)
+        size = insertHelper(intHash1Value, intHash2Value, size, first, intHash1, intHash2, next)
         return size > oldSize
     }
 
@@ -99,18 +99,18 @@ class StringHashSet(private val initialSize: Int) {
      * is str inside the map (does it exist)
      */
     fun contains(str: String): Boolean {
-        val longHashValue = stringToHashLong(str)
-        val firstIndex = (longHashValue % first.size).absoluteValue.toInt()
+        val intHash1Value = stringToHash1(str)
+        val firstIndex = (intHash1Value % first.size).absoluteValue.toInt()
         var nextIndex = first[firstIndex]
         if (nextIndex == EMPTY_KEY)
             return false
-        val intHashValue = stringToHashInt(str)
+        val intHash2Value = stringToHash2(str)
         while (next[nextIndex] != EMPTY_KEY) {
-            if (longHash[nextIndex] == longHashValue && intHash[nextIndex] == intHashValue)
+            if (intHash1[nextIndex] == intHash1Value && intHash2[nextIndex] == intHash2Value)
                 return true
             nextIndex = next[nextIndex]
         }
-        return (longHash[nextIndex] == longHashValue && intHash[nextIndex] == intHashValue)
+        return (intHash1[nextIndex] == intHash1Value && intHash2[nextIndex] == intHash2Value)
     }
 
 
@@ -122,8 +122,8 @@ class StringHashSet(private val initialSize: Int) {
         val growSize = ((oldSize * 3) / 2) + 1 // 50% growth
 
         val newFirst = IntArray(growSize) { EMPTY_KEY }
-        val newLongHash = LongArray(growSize) { EMPTY_KEY_LONG }
-        val newIntHash = IntArray(growSize) { EMPTY_KEY }
+        val newHash1Array = IntArray(growSize) { EMPTY_KEY }
+        val newHash2Array = IntArray(growSize) { EMPTY_KEY }
         val newNext = IntArray(growSize) { EMPTY_KEY }
         var newSize = 0
 
@@ -131,52 +131,37 @@ class StringHashSet(private val initialSize: Int) {
 
             var oldNextIndex = oldFirst
             while (oldNextIndex != EMPTY_KEY) {
-                val longHashValue = longHash[oldNextIndex]
-                val intHashValue = intHash[oldNextIndex]
-                newSize = insertHelper(longHashValue, intHashValue, newSize, newFirst, newLongHash, newIntHash, newNext)
+                val intHash1Value = intHash1[oldNextIndex]
+                val intHash2Value = intHash2[oldNextIndex]
+                newSize = insertHelper(intHash1Value, intHash2Value, newSize, newFirst, newHash1Array, newHash2Array, newNext)
                 oldNextIndex = next[oldNextIndex]
             }
 
         } // for each existing entry
 
         // transfer the newly mapped data
-        longHash = newLongHash
         first = newFirst
         next = newNext
-        intHash = newIntHash
+        intHash1 = newHash1Array
+        intHash2 = newHash2Array
         size = newSize
-    }
-
-
-    /**
-     * read all the hashes stored under a bucket (all colliding items)
-     */
-    private fun getHashesFromBucket(firstIndex: Int): List<Int> {
-        val hashList = mutableListOf<Int>()
-        var nextIndex = first[firstIndex]
-        while (nextIndex != EMPTY_KEY) {
-            hashList.add(intHash[nextIndex])
-            nextIndex = next[nextIndex]
-        }
-        return hashList
     }
 
 
     companion object {
         private val hashCalc = Adler32()
         private const val EMPTY_KEY = -1
-        private const val EMPTY_KEY_LONG = -1L
 
         /**
-         * return a long hash for a string that is never equal to EMPTY_KEY_LONG
+         * return a long hash for a string that is never equal to EMPTY_KEY
          * @param str the string to hash
-         * @return a value between MIN_LONG and MAX_LONG that is enver EMPTY_KEY_LONG
+         * @return a value between MIN_LONG and MAX_LONG that is enver EMPTY_KEY
          */
-        fun stringToHashLong(str: String): Long {
+        fun stringToHash1(str: String): Int {
             hashCalc.reset()
             hashCalc.update(str.toByteArray())
-            val value = hashCalc.value
-            return if (value != EMPTY_KEY_LONG) value else 0
+            val value = hashCalc.value.toInt()
+            return if (value != EMPTY_KEY) value else 0
         }
 
         /**
@@ -184,40 +169,40 @@ class StringHashSet(private val initialSize: Int) {
          * @param str the string to return a hash for
          * @return a number between MIN_INT and MAX_INT that is never EMPTY_KEY
          */
-        fun stringToHashInt(str: String): Int {
+        fun stringToHash2(str: String): Int {
             val strHash = str.hashCode()
             return if (strHash == EMPTY_KEY) 0 else strHash
         }
 
         /**
          * help insert a value into our data structure
-         * @param longHashValue the string's long hash
-         * @param intHashValue the string's int hash
+         * @param intHash1Value the string's first int hash
+         * @param intHash2Value the string's second int hash
          * @param size the number of items in the map presently
          * @param first the map of first indexes pointing into our data structures
-         * @param longHash the collection of long hashes
-         * @param intHash the collection of int hashes
+         * @param intHash1Data the collection of int hashes for the 1 values
+         * @param intHash2Data the collection of int hashes for the 2 values
          * @param next the next pointer array
          * @return the new size if a new value was inserted, or the old size if the value already existed
          */
         private fun insertHelper(
-            longHashValue: Long,
-            intHashValue: Int,
+            intHash1Value: Int,
+            intHash2Value: Int,
             size: Int,
             first: IntArray,
-            longHash: LongArray,
-            intHash: IntArray,
+            intHash1Data: IntArray,
+            intHash2Data: IntArray,
             next: IntArray
         ): Int {
-            val firstIndex = (longHashValue % first.size).absoluteValue.toInt()
+            val firstIndex = (intHash1Value % first.size).absoluteValue.toInt()
             var newSize = size
 
             // simplest case - we don't have an entry yet
             if (first[firstIndex] == EMPTY_KEY) {
                 first[firstIndex] = size // first points to the next empty data-slot
                 // and the data goes into the slots
-                longHash[size] = longHashValue
-                intHash[size] = intHashValue
+                intHash1Data[size] = intHash1Value
+                intHash2Data[size] = intHash2Value
                 next[size] = EMPTY_KEY
                 newSize += 1
 
@@ -225,17 +210,17 @@ class StringHashSet(private val initialSize: Int) {
                 // chain down the colliding items and find the next empty
                 var nextIndex = first[firstIndex]
                 while (next[nextIndex] != EMPTY_KEY) {
-                    if (intHash[nextIndex] == intHashValue) // already exists, not added
+                    if (intHash2Data[nextIndex] == intHash2Value) // already exists, not added
                         return size
                     nextIndex = next[nextIndex]
                 }
-                if (longHash[nextIndex] == longHashValue && intHash[nextIndex] == intHashValue) // already exists, not added
+                if (intHash1Data[nextIndex] == intHash1Value && intHash2Data[nextIndex] == intHash2Value) // already exists, not added
                     return size
                 // now prevNext points to the last slot that wasn't empty - chain it in
                 next[nextIndex] = size
                 // and put in our data at the end
-                longHash[size] = longHashValue
-                intHash[size] = intHashValue
+                intHash1Data[size] = intHash1Value
+                intHash2Data[size] = intHash2Value
                 next[size] = EMPTY_KEY
                 newSize += 1
             }
